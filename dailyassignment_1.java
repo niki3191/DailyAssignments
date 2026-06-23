c175947150f6a1c9a7dd134bb26ec1eefd334910
niki3191 <38745119+niki3191@users.noreply.github.com>
2026-06-23 21:40:30 +0530

    Add files via upload

---
+++ dailyassignment_1.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class DailyAssignment_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=101;
+		System.out.println("Student ID is" +num);
+		
+		char Grade ='A';
+		System.out.println("Grade is " +Grade);
+		
+		boolean passed =true;
+		System.out.println(passed);
+		
+		byte age =18;
+		System.out.println(age);
+		
+		
+	}
+
+}
